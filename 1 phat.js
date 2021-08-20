7c251c8fc4d0dbed45db7008b8d36fd8549705a5
phat <fatsandee@gmail.com>
2021-08-20 18:07:07 +0700

    Create 1 phat.js

---
+++ 1 phat.js
@@ -0,0 +1,9 @@
+let Cvalue = "";
+function Sum(){
+  //  A = document.getElementById(a).value;
+  //  B = document.getElementById(b).value;
+   Cvalue = document.getElementById("c").value;
+   D = 0;
+  //  ParsedA = parseInt(A);
+}
+  document.getElementById("R").innerHTML = Cvalue;
